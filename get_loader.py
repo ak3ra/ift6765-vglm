48492c48eb18d3b85bbafb82712cdc26ad9ebf84
ak3ra <akeraben@gmail.com>
2022-02-15 15:32:51 -0500

    barebones

--- get_loader.py
+++ get_loader.py
@@ -6,3 +6,26 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
 from pil import Image
 
+class FlickrDataset(Dataset):
+    def __init__(self, root_dir, captions_file, transform=None, freq_threshold=5):
+        self.root_dir = root_dir
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+
+        #get image-caption columns
+        self.imgs = self.df["image"]
+        self.caption = self.df["captions"]
+
+        #initialize captions and build vocabulary
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocabulary(self.captions.tolist())
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        return self.df.iloc(index)
+
+
+
+
