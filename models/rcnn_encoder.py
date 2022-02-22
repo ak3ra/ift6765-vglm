39f11bcfed152b3a2c166bc5cae2ec4510f44f64
ak3ra <akeraben@gmail.com>
2022-02-22 11:59:12 -0500

    clean up

--- models/rcnn_encoder.py
+++ models/rcnn_encoder.py
@@ -11,12 +11,14 @@ def load_model():
     resnext = torchvision.models.resnext50_32x4d(pretrained=True)
     modules = list(resnext.children())[:-1]
     backbone = nn.Sequential(*modules)
-
     backbone.out_channels = 2048
+
     anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
                                    aspect_ratios=((0.5, 1.0, 2.0),))
 
-    roi_pooler = torchvision.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
+    roi_pooler = torchvision.MultiScaleRoIAlign(featmap_names=['0'], 
+						output_size=7,
+						sampling_ratio=2)
 
     model = FasterRCNN(backbone,
                    num_channels=4,
