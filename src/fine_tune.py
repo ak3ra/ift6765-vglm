3bab579608bd2ac98aa3968f4d6be78c1174b8d4
Ge Li <ge.li2@mail.mcgill.ca>
2022-04-06 22:27:14 -0400

    with metrics

--- src/fine_tune.py
+++ src/fine_tune.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, DataCollatorWithPadding
 from transformers import TrainingArguments,AutoModelForSequenceClassification,Trainer
 
-task='sst2'
+task='mrpc'
 checkpoint = "bert-base-uncased"
 # checkpoint = './pre_trained_model'
 train_batch_size = 128
@@ -32,13 +32,28 @@ elif task == 'stsb':
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
+import numpy as np
+from datasets import load_metric
+
+def compute_metrics(eval_preds):
+    metric = load_metric("glue", "mrpc")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
 training_args = TrainingArguments("test-trainer",
                 per_device_train_batch_size = train_batch_size,
                 per_device_eval_batch_size = eval_batch_size)
 
 
-
+# trainer = Trainer(
+#     model,
+#     training_args,
+#     train_dataset=tokenized_datasets["train"],
+#     eval_dataset=tokenized_datasets["validation"],
+#     data_collator=data_collator,
+#     tokenizer=tokenizer,
+# )
 
 trainer = Trainer(
     model,
@@ -47,9 +62,7 @@ trainer = Trainer(
     eval_dataset=tokenized_datasets["validation"],
     data_collator=data_collator,
     tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
 )
-
 trainer.train()
 
-
-python ../run_glue.py \\n  --model_name_or_path bert-base-cased \\n  --task_name stsb \\n  --do_train \\n  --do_eval \\n  --max_seq_length 512 \\n  --per_device_train_batch_size 16 \\n  --learning_rate 2e-5 \\n  --num_train_epochs 3 \\n  --output_dir bert-base-cased-finetuned-stsb \\n  --push_to_hub \\n  --hub_strategy all_checkpoints \\n  --logging_strategy epoch \\n  --save_strategy epoch \\n  --evaluation_strategy epoch \\n```
